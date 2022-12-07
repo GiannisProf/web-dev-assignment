16a524198fe946f7ea77bff52ca2d28f1ad552ff
Giannis Papachristou <giannis.papachristou@yahoo.gr>
2022-12-07 21:52:49 +0200

    incompleted changes on main.js | added phone effect

---
+++ assignment-1/Exercise5/main.js
@@ -0,0 +1,59 @@
+document.getElementById("first_name").setAttribute("placeholder", "Name");
+document.getElementById("last_name").setAttribute("placeholder", "Surname");
+
+const tel1 = document.getElementById("txt_telephone");
+tel1.addEventListener('input', () => {
+    tel1.setCustomValidity('');
+    tel1.checkValidity();
+})
+
+tel1.addEventListener('invalid', () => {
+        tel1.setCustomValidity('The phone pattern is not matched. You should try something like: 6938823619. ' +
+            'Please try again!');
+    }
+);
+
+const tel2 = document.getElementById("txt_telephone2");
+tel2.addEventListener('input', () => {
+    tel2.setCustomValidity('');
+    tel2.checkValidity();
+})
+
+tel2.addEventListener('invalid', () => {
+    tel2.setCustomValidity('The phone pattern is not matched. You should try something like: 6938823619. ' +
+            'Please try again!');
+    }
+);
+
+const fname =  document.getElementById("first_name");
+fname.addEventListener('input', () => {
+    fname.setCustomValidity('');
+    fname.checkValidity();
+});
+
+fname.addEventListener('invalid', () => {
+    fname.setCustomValidity('Please fill in your name.');
+    fname.style.backgroundColor = "rgb(255,97,97)";
+});
+
+const lname =  document.getElementById("last_name");
+lname.addEventListener('input', () => {
+    lname.setCustomValidity('');
+    lname.checkValidity();
+});
+
+lname.addEventListener('invalid', () => {
+    lname.setCustomValidity('Please fill in your surname.');
+    lname.style.backgroundColor = "rgb(255,97,97)";
+});
+
+const mail = document.getElementById("my_email");
+mail.addEventListener('input', () => {
+    mail.setCustomValidity('');
+    mail.checkValidity();
+});
+
+mail.addEventListener('invalid', () => {
+        mail.setCustomValidity('This is not an accurate e-mail address. Please try again!');
+    }
+);
